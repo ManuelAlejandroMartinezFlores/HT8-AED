e85b95d50ea0aa96f3b2990ac22878ca45f56d6a
Manuel <manuelalejandromartinezf@gmail.com>
2022-04-07 08:17:35 -0600

    MVC

--- HT8/src/Archivos.java
+++ HT8/src/Archivos.java
@@ -1,8 +1,6 @@
-import java.util.HashMap;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
-import java.util.ArrayList;
 
 public class Archivos{
 
---
+++ HT8/src/Controlador.java
@@ -0,0 +1,23 @@
+public class Controlador {
+    
+    public static void main(String[] args){
+        Vista vista = new Vista();
+
+        PriorityQueue<Paciente> pq;
+        while (true){
+            try{
+                pq = Archivos.leer(vista.getPath());
+                break;
+            } catch (Exception e){
+                vista.mostrarMsg("\nError al leer el archivo\n");
+            }
+        }
+
+        while (!pq.isEmpty()){
+            vista.getPaciente();
+            vista.mostrarMsg(pq.remove().toString() + "\n");
+        }
+
+        vista.mostrarMsg("Cerrando...");
+    }
+}
