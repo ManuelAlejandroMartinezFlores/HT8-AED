8541a10e8d9f4d613d185a5dbb7b2bd5b7ad03c1
Manuel <manuelalejandromartinezf@gmail.com>
2022-04-07 08:19:22 -0600

    Archivos

---
+++ HT8 2da Version/src/Archivos.java
@@ -0,0 +1,20 @@
+import java.util.PriorityQueue;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+public class Archivos {
+    
+    public static PriorityQueue<Paciente> leer(String path) throws IOException{
+        PriorityQueue<Paciente> pq = new PriorityQueue<Paciente>();
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        String row;
+        while ((row = reader.readLine()) != null){
+        	String[] data = row.split(", ");
+            pq.add(new Paciente(data[0], data[1], data[2]));
+        }
+        reader.close();
+        return pq;
+    }
+
+}
